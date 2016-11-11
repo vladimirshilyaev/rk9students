6a0862d29af25b1bff590630e03ff91a24c985e4
arid1995 <arid1995@mail.ru>
2016-11-12 01:10:08 +0300

    User email is shown

--- code/src/webapp/MedCad/MedCad/ClientApp/app/services/auth/auth.service.ts
+++ code/src/webapp/MedCad/MedCad/ClientApp/app/services/auth/auth.service.ts
@@ -18,10 +18,25 @@ export class Auth {
     // Configure Auth0
     lock = new Auth0Lock('wFZh1IypbUquG9MyaG1nvWLeB2kzwiU5', 'aridlife.auth0.com', options);
 
+    userProfile: Object;
+
     constructor() {
+        // Set userProfile attribute of already saved profile
+        this.userProfile = JSON.parse(localStorage.getItem('profile'));
         // Add callback for lock `authenticated` event
         this.lock.on("authenticated", (authResult) => {
             localStorage.setItem('id_token', authResult.idToken);
+
+            this.lock.getProfile(authResult.idToken, (error, profile) => {
+                if (error) {
+                    // Handle error
+                    alert(error);
+                    return;
+                }
+
+                localStorage.setItem('profile', JSON.stringify(profile));
+                this.userProfile = profile;
+            });
         });
     }
 
@@ -39,5 +54,7 @@ export class Auth {
     public logout() {
         // Remove token from localStorage
         localStorage.removeItem('id_token');
+        localStorage.removeItem('profile');
+        this.userProfile = undefined;
     };
 }
