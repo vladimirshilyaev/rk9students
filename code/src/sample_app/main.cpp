7d981f76403a3a9bae0e1ad46e8bf39dc9b12353
Sergey Khrykov <s.khrykov@gmail.com>
2016-11-17 22:28:53 +0300

    Minor change in sample_app

--- code/src/sample_app/main.cpp
+++ code/src/sample_app/main.cpp
@@ -16,7 +16,13 @@ int main(int argc, char* argv[])
 
 	model_a.ReadFromSTLFile(argv[1]);
 
-	model_b.ReadFromSTLFile(argv[1]);
+	for (size_t i = 0; i < model_a.GetTrianglesCount(); ++i) {
+		auto t = model_a.GetTriangleVertices(i);
+		t.A.X += 300; // translate points in X direction
+		t.B.X += 300;
+		t.C.X += 300;
+		model_b.AddTriangle(t);
+	}
 
 	PolyModel* res = PolyModel::MergeModels(model_a, model_b);
 
